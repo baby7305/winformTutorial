c7494a99aa52384a0854fd19d3a96179edf9c064
宋海贝 <songhaibei@shmingjiang.com.cn>
2018-01-28 14:02:11 +0800

    树形结构测试

--- WindowsFormsApplication1/MForm.cs
+++ WindowsFormsApplication1/MForm.cs
@@ -10,95 +10,46 @@ using System.Windows.Forms;
 
 namespace WindowsFormsApplication1
 {
-    public class Actress
-    {
-        public string name;
-        public int year;
-
-        public Actress(string name, int year)
-        {
-            this.name = name;
-            this.year = year;
-        }
-    }
-
     partial class MForm : Form
     {
 
-        private StatusBar sb;
+        StatusBar sb;
 
         public MForm()
         {
-            Text = "ListView";
-            Size = new Size(350, 300);
+            Text = "TreeView";
+            Size = new Size(250, 250);
 
+            TreeView tv = new TreeView();
 
-            List<Actress> actresses = new List<Actress>();
+            TreeNode root = new TreeNode();
+            root.Text = "Languages";
 
-            actresses.Add(new Actress("Jessica Alba", 1981));
-            actresses.Add(new Actress("Angelina Jolie", 1975));
-            actresses.Add(new Actress("Natalie Portman", 1981));
-            actresses.Add(new Actress("Rachel Weiss", 1971));
-            actresses.Add(new Actress("Scarlett Johansson", 1984));
+            TreeNode child1 = new TreeNode();
+            child1.Text = "Python";
 
+            TreeNode child2 = new TreeNode();
+            child2.Text = "Ruby";
 
-            ColumnHeader name = new ColumnHeader();
-            name.Text = "Name";
-            name.Width = -1;
-            ColumnHeader year = new ColumnHeader();
-            year.Text = "Year";
+            TreeNode child3 = new TreeNode();
+            child3.Text = "Java";
 
-            SuspendLayout();
+            root.Nodes.AddRange(new TreeNode[] { child1, child2, child3 });
 
-            ListView lv = new ListView();
-            lv.Parent = this;
-            lv.FullRowSelect = true;
-            lv.GridLines = true;
-            lv.AllowColumnReorder = true;
-            lv.Sorting = SortOrder.Ascending;
-            lv.Columns.AddRange(new ColumnHeader[] { name, year });
-            lv.ColumnClick += new ColumnClickEventHandler(ColumnClick);
-
-            foreach (Actress act in actresses)
-            {
-                ListViewItem item = new ListViewItem();
-                item.Text = act.name;
-                item.SubItems.Add(act.year.ToString());
-                lv.Items.Add(item);
-            }
-
-            lv.Dock = DockStyle.Fill;
-            lv.Click += new EventHandler(OnChanged);
+            tv.Parent = this;
+            tv.Nodes.Add(root);
+            tv.Dock = DockStyle.Fill;
+            tv.AfterSelect += new TreeViewEventHandler(AfterSelect);
 
             sb = new StatusBar();
             sb.Parent = this;
-            lv.View = View.Details;
-
-            ResumeLayout();
 
             CenterToScreen();
         }
 
-        void OnChanged(object sender, EventArgs e)
+        void AfterSelect(object sender, TreeViewEventArgs e)
         {
-            ListView lv = (ListView)sender;
-            string name = lv.SelectedItems[0].SubItems[0].Text;
-            string born = lv.SelectedItems[0].SubItems[1].Text;
-            sb.Text = name + ", " + born;
-        }
-
-        void ColumnClick(object sender, ColumnClickEventArgs e)
-        {
-            ListView lv = (ListView)sender;
-
-            if (lv.Sorting == SortOrder.Ascending)
-            {
-                lv.Sorting = SortOrder.Descending;
-            }
-            else
-            {
-                lv.Sorting = SortOrder.Ascending;
-            }
+            sb.Text = e.Node.Text;
         }
     }
 }
