4a6177f6dba3c67ca362c187c74a03fa307cbb8d
宋海贝 <songhaibei@shmingjiang.com.cn>
2018-01-28 13:04:45 +0800

    音乐播放器测试

--- WindowsFormsApplication1/MForm.cs
+++ WindowsFormsApplication1/MForm.cs
@@ -13,44 +13,88 @@ namespace WindowsFormsApplication1
     partial class MForm : Form
     {
 
-        private int WIDTH = 250;
-        private int HEIGHT = 150;
-        private int BUTTONS_SPACE = 15;
-        private int PANEL_SPACE = 8;
-        private int CLOSE_SPACE = 10;
 
         public MForm()
         {
-            Text = "Anchor";
-            Size = new Size(WIDTH, HEIGHT);
+            Text = "Player";
+            Size = new Size(350, 280);
 
-            Button ok = new Button();
+            MainMenu mainMenu = new MainMenu();
+            MenuItem file = mainMenu.MenuItems.Add("&File");
+            MenuItem playm = mainMenu.MenuItems.Add("&Play");
+            MenuItem view = mainMenu.MenuItems.Add("&View");
 
-            int PANEL_HEIGHT = ok.Height + PANEL_SPACE;
+            MenuItem tools = mainMenu.MenuItems.Add("&Tools");
+            MenuItem favourites = mainMenu.MenuItems.Add("&Favourites");
+            MenuItem help = mainMenu.MenuItems.Add("&Help");
+            file.MenuItems.Add(new MenuItem("E&xit",
+                new EventHandler(this.OnExit), Shortcut.CtrlX));
+
+            Menu = mainMenu;
 
             Panel panel = new Panel();
-            panel.Height = PANEL_HEIGHT;
-            panel.Dock = DockStyle.Bottom;
             panel.Parent = this;
+            panel.BackColor = Color.Black;
+            panel.Dock = DockStyle.Fill;
 
-            int x = ok.Width * 2 + BUTTONS_SPACE;
-            int y = (PANEL_HEIGHT - ok.Height) / 2;
+            Panel buttonPanel = new Panel();
+            buttonPanel.Parent = this;
+            buttonPanel.Height = 40;
+            buttonPanel.Dock = DockStyle.Bottom;
 
-            ok.Text = "Ok";
-            ok.Parent = panel;
-            ok.Location = new Point(WIDTH - x, y);
-            ok.Anchor = AnchorStyles.Right;
+            Button pause = new Button();
+            pause.FlatStyle = FlatStyle.Popup;
+            pause.Parent = buttonPanel;
+            pause.Location = new Point(5, 10);
+            pause.Size = new Size(25, 25);
+            pause.Image = new Bitmap("res/pause.png");
 
-            Button close = new Button();
+            Button play = new Button();
+            play.FlatStyle = FlatStyle.Popup;
+            play.Parent = buttonPanel;
+            play.Location = new Point(35, 10);
+            play.Size = new Size(25, 25);
+            play.Image = new Bitmap("res/play.png");
 
-            x = close.Width;
+            Button forward = new Button();
+            forward.FlatStyle = FlatStyle.Popup;
+            forward.Parent = buttonPanel;
+            forward.Location = new Point(80, 10);
+            forward.Size = new Size(25, 25);
+            forward.Image = new Bitmap("res/forward.png");
 
-            close.Text = "Close";
-            close.Parent = panel;
-            close.Location = new Point(WIDTH - x - CLOSE_SPACE, y);
-            close.Anchor = AnchorStyles.Right;
+            Button backward = new Button();
+            backward.FlatStyle = FlatStyle.Popup;
+            backward.Parent = buttonPanel;
+            backward.Location = new Point(110, 10);
+            backward.Size = new Size(25, 25);
+            backward.Image = new Bitmap("res/backward.png");
+
+            TrackBar tb = new TrackBar();
+            tb.Parent = buttonPanel;
+            tb.TickStyle = TickStyle.None;
+            tb.Size = new Size(150, 25);
+            tb.Location = new Point(200, 10);
+            tb.Anchor = AnchorStyles.Right;
+
+            Button audio = new Button();
+            audio.FlatStyle = FlatStyle.Popup;
+            audio.Parent = buttonPanel;
+            audio.Size = new Size(25, 25);
+            audio.Image = new Bitmap("res/audio.png");
+            audio.Location = new Point(170, 10);
+            audio.Anchor = AnchorStyles.Right;
+
+            StatusBar sb = new StatusBar();
+            sb.Parent = this;
+            sb.Text = "Ready";
 
             CenterToScreen();
+        }
+
+        void OnExit(object sender, EventArgs e)
+        {
+            Close();
         }
     }
 }
