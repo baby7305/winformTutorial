c10a457ac8ab487959718a5cdc3443cbd09a380a
宋海贝 <songhaibei@shmingjiang.com.cn>
2018-01-28 13:11:36 +0800

    重命名文件

--- WindowsFormsApplication1/MForm.cs
+++ WindowsFormsApplication1/MForm.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace WindowsFormsApplication1
 {
-    public partial class Form1 : Form
+    public partial class MForm : Form
     {
-        public Form1()
+        public MForm()
         {
             Text = "Form1";
             Size = new Size(250, 200);
