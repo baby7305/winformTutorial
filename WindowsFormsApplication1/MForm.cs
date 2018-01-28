5275c51e97f229bd663df3b0f51eb11d152cec7a
宋海贝 <songhaibei@shmingjiang.com.cn>
2018-01-28 14:18:52 +0800

    颜色选择框

--- WindowsFormsApplication1/MForm.cs
+++ WindowsFormsApplication1/MForm.cs
@@ -15,35 +15,60 @@ namespace WindowsFormsApplication1
 
         private ToolBar toolbar;
         private ToolBarButton open;
-        private StatusBar statusbar;
+        private Color color;
+
+        private int rectWidth = 100;
+        private int rectHeight = 100;
+        private Rectangle r;
 
         public MForm()
         {
-            Text = "FolderBrowserDialog";
+            Text = "ColorDialog";
+
+            color = Color.Blue;
 
             toolbar = new ToolBar();
-            open = new ToolBarButton("File");
-
-            statusbar = new StatusBar();
-            statusbar.Parent = this;
+            open = new ToolBarButton("color");
 
             toolbar.Buttons.Add(open);
             toolbar.ButtonClick += new ToolBarButtonClickEventHandler(OnClicked);
 
+            LocateRect();
+
+            SetStyle(ControlStyles.ResizeRedraw, true);
             Controls.Add(toolbar);
+            Paint += new PaintEventHandler(OnPaint);
 
             CenterToScreen();
         }
 
 
+        void OnPaint(object sender, PaintEventArgs e)
+        {
+            Graphics g = e.Graphics;
+            LocateRect();
+
+            SolidBrush brush = new SolidBrush(color);
+
+            g.FillRectangle(brush, r);
+        }
+
+
         void OnClicked(object sender, ToolBarButtonClickEventArgs e)
         {
-            FolderBrowserDialog dialog = new FolderBrowserDialog();
-
+            ColorDialog dialog = new ColorDialog();
             if (dialog.ShowDialog(this) == DialogResult.OK)
             {
-                statusbar.Text = dialog.SelectedPath;
+                color = dialog.Color;
+                Invalidate();
             }
+        }
+
+        void LocateRect()
+        {
+            int x = (ClientSize.Width - rectWidth) / 2;
+            int y = (ClientSize.Height - rectHeight) / 2;
+            r = new Rectangle(x, y, rectWidth, rectHeight);
         }
     }
 }
