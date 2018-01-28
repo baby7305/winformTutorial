018804e1fecfa9640a6be2b24491dd03846c92bb
宋海贝 <songhaibei@shmingjiang.com.cn>
2018-01-28 13:49:36 +0800

    方框打勾确定测试

--- WindowsFormsApplication1/MForm.cs
+++ WindowsFormsApplication1/MForm.cs
@@ -13,39 +13,34 @@ namespace WindowsFormsApplication1
     partial class MForm : Form
     {
 
-        string text = @"Sometimes I feel I've got to
-Run away I've got to
-Get away
-From the pain that you drive
-    into the heart of me
-The love we share
-Seems to go nowhere
-I've lost my lights
-I toss and turn I can't sleep at night
-
-Once I ran to you (I ran)
-Now I'll run from you
-This tainted love you've given
-I give you all a boy could give you
-Take my tears and that's not nearly all
-Tainted love
-Tainted love";
+        private CheckBox cb;
 
         public MForm()
         {
-            Text = "Tainted Love";
+            Text = "CheckBox";
+            Size = new Size(220, 170);
 
-            Font font = new Font("Serif", 10);
+            cb = new CheckBox();
+            cb.Parent = this;
+            cb.Location = new Point(30, 30);
+            cb.Text = "Show Title";
+            cb.Checked = true;
 
-            Label lyrics = new Label();
-            lyrics.Parent = this;
-            lyrics.Text = text;
-            lyrics.Font = font;
-            lyrics.Location = new Point(10, 10);
-            lyrics.Size = new Size(290, 290);
+            cb.CheckedChanged += new EventHandler(OnChanged);
 
             CenterToScreen();
+        }
 
+        void OnChanged(object sender, EventArgs e)
+        {
+            if (cb.Checked)
+            {
+                Text = "CheckBox";
+            }
+            else
+            {
+                Text = "";
+            }
         }
     }
 }
