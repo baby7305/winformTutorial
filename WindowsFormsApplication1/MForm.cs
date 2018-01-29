f62d08aff69db27e212c91765fd63d31b4bb64aa
宋海贝 <songhaibei@shmingjiang.com.cn>
2018-01-29 10:10:39 +0800

    测试直线

--- WindowsFormsApplication1/MForm.cs
+++ WindowsFormsApplication1/MForm.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.Drawing.Drawing2D;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,13 +11,41 @@ using System.Windows.Forms;
 
 namespace WindowsFormsApplication1
 {
-    public partial class MForm : Form
+    partial class MForm : Form
     {
+
         public MForm()
         {
-            Text = "Form1";
-            Size = new Size(250, 200);
+            Text = "Lines";
+            Size = new Size(280, 270);
+            ResizeRedraw = true;
+
+            Paint += new PaintEventHandler(OnPaint);
             CenterToScreen();
+        }
+
+
+        void OnPaint(object sender, PaintEventArgs e)
+        {
+            Graphics g = e.Graphics;
+            Pen pen = new Pen(Color.Black, 1);
+
+            pen.DashStyle = DashStyle.Dot;
+            g.DrawLine(pen, 20, 40, 250, 40);
+
+            pen.DashStyle = DashStyle.DashDot;
+            g.DrawLine(pen, 20, 80, 250, 80);
+
+            pen.DashStyle = DashStyle.Dash;
+            g.DrawLine(pen, 20, 120, 250, 120);
+
+            pen.DashStyle = DashStyle.DashDotDot;
+            g.DrawLine(pen, 20, 160, 250, 160);
+
+            pen.DashPattern = new float[] { 6f, 8f, 1f, 1f, 1f, 1f, 1f, 1f };
+            g.DrawLine(pen, 20, 200, 250, 200);
+
+            g.Dispose();
         }
     }
 }
