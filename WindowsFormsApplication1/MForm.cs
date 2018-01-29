e4ff741f8abea97a37aea283278e5381eab36ed1
宋海贝 <songhaibei@shmingjiang.com.cn>
2018-01-29 10:22:41 +0800

    绘制加载图片

--- WindowsFormsApplication1/MForm.cs
+++ WindowsFormsApplication1/MForm.cs
@@ -14,61 +14,38 @@ namespace WindowsFormsApplication1
     partial class MForm : Form
     {
 
+        private Bitmap castle;
+
         public MForm()
         {
-
-            Text = "You know I'm No Good";
-            Size = new Size(380, 450);
+            Text = "Red Rock";
+            loadImage();
+            ClientSize = new Size(castle.Width, castle.Height);
 
             Paint += new PaintEventHandler(OnPaint);
+
             CenterToScreen();
         }
 
+        void loadImage()
+        {
+            try
+            {
+                castle = new Bitmap("res/redrock.png");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                Environment.Exit(1);
+            }
+        }
 
         void OnPaint(object sender, PaintEventArgs e)
         {
             Graphics g = e.Graphics;
 
-            Font ft = new Font("Purisa", 10);
-            SolidBrush br = new SolidBrush(Color.Black);
-
-            PointF pt = new PointF(20.0f, 20.0f);
-            g.DrawString("Meet you downstairs in the bar and heard", ft, br, pt);
-
-            pt = new PointF(20.0f, 50.0f);
-            g.DrawString("Your rolled up sleeves and your skull t-shirt", ft, br, pt);
-
-            pt = new PointF(20.0f, 80.0f);
-            g.DrawString("You say why did you do it with him today?", ft, br, pt);
-
-            pt = new PointF(20.0f, 110.0f);
-            g.DrawString("And sniffed me out like I was tanqueray", ft, br, pt);
-
-            pt = new PointF(20.0f, 160.0f);
-            g.DrawString("Cause you’re my fella, my guy", ft, br, pt);
-
-            pt = new PointF(20.0f, 190.0f);
-            g.DrawString("Hand me your stella and fly", ft, br, pt);
-
-            pt = new PointF(20.0f, 220.0f);
-            g.DrawString("By the time I’m out the door", ft, br, pt);
-
-            pt = new PointF(20.0f, 250.0f);
-            g.DrawString("You tear me down like roger moore", ft, br, pt);
-
-            pt = new PointF(20.0f, 300.0f);
-            g.DrawString("I cheated myself", ft, br, pt);
-
-            pt = new PointF(20.0f, 330.0f);
-            g.DrawString("Like I knew I would", ft, br, pt);
-
-            pt = new PointF(20.0f, 360.0f);
-            g.DrawString("I told ya, I was trouble", ft, br, pt);
-
-            pt = new PointF(20.0f, 390.0f);
-            g.DrawString("You know that I’m no good", ft, br, pt);
-
-            g.Dispose();
+            Rectangle r = new Rectangle(1, 1, castle.Width, castle.Height);
+            g.DrawImage(castle, r);
         }
     }
 }
