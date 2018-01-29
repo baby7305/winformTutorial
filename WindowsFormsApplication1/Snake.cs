c38dce8321ae9a8b190f47c37ba2aaddc4788966
宋海贝 <songhaibei@shmingjiang.com.cn>
2018-01-29 10:50:10 +0800

    贪吃蛇游戏测试

---
+++ WindowsFormsApplication1/Snake.cs
@@ -0,0 +1,27 @@
+﻿using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    class Snake : Form
+    {
+
+        public Snake()
+        {
+
+            Text = "Snake";
+            DoubleBuffered = true;
+            FormBorderStyle = FormBorderStyle.FixedSingle;
+
+            int borderWidth = (this.Width - this.ClientSize.Width) / 2;
+            int titleBarHeight = this.Height - this.ClientSize.Height - borderWidth;
+
+            Board board = new Board();
+            board.BORDER_WIDTH = borderWidth;
+            board.TITLEBAR_HEIGHT = titleBarHeight;
+
+            Controls.Add(board);
+            CenterToScreen();
+
+        }
+    }
+}
