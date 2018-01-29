658f5289ef8ca40cae3919019497bd2661079558
宋海贝 <songhaibei@shmingjiang.com.cn>
2018-01-29 09:56:42 +0800

    拖放按钮

--- WindowsFormsApplication1/MForm.cs
+++ WindowsFormsApplication1/MForm.cs
@@ -12,11 +12,47 @@ namespace WindowsFormsApplication1
 {
     public partial class MForm : Form
     {
+        private bool isDragging = false;
+        private int oldX, oldY;
+        private Button button;
+
         public MForm()
         {
-            Text = "Form1";
-            Size = new Size(250, 200);
+            Text = "Drag & drop button";
+            Size = new Size(270, 180);
+
+            button = new Button();
+            button.Parent = this;
+            button.Cursor = Cursors.Hand;
+            button.Text = "Button";
+            button.Location = new Point(20, 20);
+
+            button.MouseDown += new MouseEventHandler(OnMouseDown);
+            button.MouseUp += new MouseEventHandler(OnMouseUp);
+            button.MouseMove += new MouseEventHandler(OnMouseMove);
+
             CenterToScreen();
+        }
+
+        private void OnMouseDown(object sender, MouseEventArgs e)
+        {
+            isDragging = true;
+            oldX = e.X;
+            oldY = e.Y;
+        }
+
+        private void OnMouseMove(object sender, MouseEventArgs e)
+        {
+            if (isDragging)
+            {
+                button.Top = button.Top + (e.Y - oldY);
+                button.Left = button.Left + (e.X - oldX);
+            }
+        }
+
+        private void OnMouseUp(object sender, MouseEventArgs e)
+        {
+            isDragging = false;
         }
     }
 }
